4b34308dc3d90774c924e8ce7e143e92fdf00058
leejihoon <ljhljh211@naver.com>
2023-10-22 20:22:52 +0900

    test: 예시 페이지 추가

--- src/pages/test/testStyle.tsx
+++ src/pages/test/testStyle.tsx
@@ -1,14 +1,25 @@
-import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 export default function TestStylePage() {
   return (
     <Area>
-      TestStylePage <div css={{ color: "red" }}>test</div>
+      TestStylePage{" "}
+      <div
+        css={css`
+          color: blue;
+        `}
+      >
+        test
+      </div>
     </Area>
   );
 }
 
 const Area = styled.div`
+  width: 100%;
+  max-width: 700px;
+  margin: 0 auto;
   border: 1px solid red;
+  color: purple;
 `;
