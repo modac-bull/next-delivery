346575b6bffb3a1187a63f0f5173f4ac2857953b
leejihoon <ljhljh211@naver.com>
2023-10-23 23:24:26 +0900

    feat: Layout 컴포넌트 추가

---
+++ src/components/layouts/LayoutDefault.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled/macro";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function LayoutDefault({ children }: Props) {
+  return <LayoutContainer>{children}</LayoutContainer>;
+}
+
+const LayoutContainer = styled.div`
+  width: 100%;
+  max-width: 750px;
+  margin: 0 auto;
+  border: 1px solid #eee; // 임시
+`;
